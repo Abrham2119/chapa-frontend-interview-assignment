52c4cc33faf02446071e5689b48502c03fde6eb8
abrhamzewdu2119 <abrhamzewdu2119@gmail.com>
2025-07-10 15:07:12 +0300

    fix deployment issue

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'export', // Outputs a Single-Page Application (SPA).
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
+  experimental: {
+    reactCompiler: true,
   },
-};
+}
+ 
+export default nextConfig
