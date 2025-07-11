8c92b5729990e171863da36f2200de1b025f053b
abrhamzewdu2119 <abrhamzewdu2119@gmail.com>
2025-07-11 04:21:25 +0300

    fix:testing the login

--- pages/api/stats.ts
+++
@@ -1,6 +0,0 @@
-import { stats } from '@/services/mocks/stats';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json(stats, { status: 200 });
-}
--- pages/api/users.ts
+++
@@ -1,6 +0,0 @@
-import { users } from '@/services/mocks/users';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json(users, { status: 200 });
-}
