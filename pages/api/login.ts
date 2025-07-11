5b83ae788c31507b51f06ccdbcb3ee6b7c58d6c9
abrhamzewdu2119 <abrhamzewdu2119@gmail.com>
2025-07-11 04:15:10 +0300

    fix:testing login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,17 @@
-
-import { users } from '@/services/mocks/users';
 import { NextRequest, NextResponse } from 'next/server';
+
+const users = [
+  { email: 'john@gmail.com', password: 'userpass', role: 'User' },
+  { email: 'admin@gmail.com', password: 'adminpass', role: 'Admin' },
+  { email: 'super@gmail.com', password: 'superpass', role: 'Super_Admin' },
+];
 
 export async function POST(req: NextRequest) {
   const { email, password } = await req.json();
   const user = users.find((u) => u.email === email && u.password === password);
 
   if (user) {
-    return NextResponse.json({ email, role: user.role }, { status: 200 });
+    return NextResponse.json({ email: user.email, role: user.role }, { status: 200 });
   }
 
   return NextResponse.json({ error: 'Invalid email or password' }, { status: 401 });
